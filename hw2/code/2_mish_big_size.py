5ea057b07db934924c8f2fe3366975ebec98d046
wty-yy <wty-yy@github.com>
2025-01-04 22:00:27 +0800

    update

--- hw2/code/2_mish_big_size.py
+++ hw2/code/2_mish_big_size.py
@@ -7,11 +7,9 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard.writer import SummaryWriter
-from torchvision.models import resnet
-from torchvision.transforms.autoaugment import AutoAugment, AutoAugmentPolicy
 
 # Tensorbaord日志
-path_log = Path(f"./logs/{time.strftime('%Y%m%d-%H%M%S')}-resnet-mini")
+path_log = Path(f"./logs/{time.strftime('%Y%m%d-%H%M%S')}")
 writer = SummaryWriter(path_log)
 
 # 检查是否有可用 GPU
@@ -19,32 +17,13 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 # 超参数设置
-batch_size = 128
-learning_rate = 0.1
-momentum = 0.9
-weight_decay = 1e-4
-num_epochs = 120
-autoaugment = True
-
-# 数据增强和预处理
-# transform_train = transforms.Compose([
-#   transforms.RandomCrop(32, padding=4),
-#   transforms.RandomHorizontalFlip(),
-#   *([AutoAugment(AutoAugmentPolicy.CIFAR10)] if autoaugment else []),
-#   transforms.ToTensor(),
-#   transforms.Normalize((0.4914, 0.4822, 0.4465),
-#                       (0.2023, 0.1994, 0.2010)),
-# ])
-
-# transform_test = transforms.Compose([
-#   transforms.ToTensor(),
-#   transforms.Normalize((0.5071, 0.4867, 0.4408),
-#                        (0.2675, 0.2565, 0.2761)),
-# ])
+batch_size = 64
+learning_rate = 0.001
+num_epochs = 20
 
 # 数据增强和预处理
 transform_train = transforms.Compose([
-  transforms.RandomResizedCrop(size=160, scale=(0.6,1.0)),
+  transforms.RandomCrop(32, padding=4),      # 随机裁剪
   transforms.RandomHorizontalFlip(),      # 随机水平翻转
   transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # 色彩抖动
   transforms.ToTensor(),
@@ -52,7 +31,6 @@ transform_train = transforms.Compose([
 ])
 
 transform_test = transforms.Compose([
-  transforms.Resize(200),
   transforms.ToTensor(),
   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
@@ -60,15 +38,47 @@ transform_test = transforms.Compose([
 train_dataset = datasets.CIFAR10(root='./data', train=True, transform=transform_train, download=True)
 test_dataset = datasets.CIFAR10(root='./data', train=False, transform=transform_test, download=True)
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=8)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+class CNN(nn.Module):
+  def __init__(self, in_ch, out_ch, kernel, stride, padding):
+    super().__init__()
+    self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=kernel, stride=stride, padding=padding)
+    self.bn = nn.BatchNorm2d(out_ch)
+    self.mish = nn.Mish()
+  
+  def forward(self, x):
+    return self.mish(self.bn(self.conv(x)))
+
+# 定义 CNN 模型
+class Model(nn.Module):
+  def __init__(self, num_classes=10):
+    super().__init__()
+    self.backbone = nn.Sequential(
+      CNN(3, 64, 3, 1, 1),
+      nn.MaxPool2d(kernel_size=2, stride=2),
+      CNN(64, 128, 3, 1, 1),
+      nn.MaxPool2d(kernel_size=2, stride=2),
+      CNN(128, 256, 3, 1, 1),
+      nn.MaxPool2d(kernel_size=2, stride=2),
+    )
+    self.head = nn.Sequential(
+      nn.Linear(256 * 4 * 4, 512),
+      nn.Mish(),
+      nn.Linear(512, num_classes),
+    )
+
+  def forward(self, x):
+    x = self.backbone(x)
+    x = nn.Flatten()(x)
+    x = self.head(x)
+    return x
 
 # 初始化模型、损失函数和优化器
-# model = resnet._resnet(resnet.Bottleneck, [3, 5, 6, 3], None, True, num_classes=10).to(device)
-model = resnet._resnet(resnet.Bottleneck, [2, 3, 3, 2], None, True, num_classes=10).to(device)
+model = Model().to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=weight_decay)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader) * num_epochs)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 global_step = 0
 best_eval_acc = 0
 
@@ -88,13 +98,11 @@ for epoch in range(num_epochs):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    scheduler.step()
     global_step += 1
 
     if (batch_idx + 1) % 100 == 0:
       writer.add_scalar("chart/loss", loss.item(), global_step)
       writer.add_scalar("chart/train_acc", acc, global_step)
-      writer.add_scalar("chart/learning_rate", scheduler.get_last_lr()[0], global_step)
       print(f"Epoch [{epoch + 1}/{num_epochs}], Step [{batch_idx + 1}/{len(train_loader)}], Loss: {loss.item():.4f}, Acc: {acc:.4f}")
 
   # 测试模型
@@ -116,12 +124,12 @@ for epoch in range(num_epochs):
   if eval_acc > best_eval_acc:
     best_eval_acc = eval_acc
     # 保存最优eval模型
-    path_save_model = f"cifar10_resnet_model_best_eval.pth"
+    path_save_model = f"cifar10_cnn_model_best_eval.pth"
     torch.save(model.state_dict(), path_log / path_save_model)
     print(f"Best eval model ({100*eval_acc:.2f}%) saved as {path_log / path_save_model}")
 
 # 保存模型
-path_save_model = f"cifar10_resnet_model_{global_step}.pth"
+path_save_model = f"cifar10_cnn_model_{global_step}.pth"
 torch.save(model.state_dict(), path_log / path_save_model)
 print(f"Last model saved as {path_log / path_save_model}")
 print(f"Best eval accuracy {100 * best_eval_acc:.2f}%")
